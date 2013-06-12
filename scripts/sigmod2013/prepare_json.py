c17f84a00c0c8720fac4d724affa7a6e5b19321b
Anant Bhardwaj <anantb@csail.mit.edu>
2013-06-12 07:46:26 -0700

    final schedule

--- scripts/sigmod2013/prepare_json.py
+++ scripts/sigmod2013/prepare_json.py
@@ -1,4 +1,4 @@
-import sys, os, json, csv, re
+import sys, os, json, csv, re, difflib
 
 if __name__ == "__main__":
 	p = os.path.abspath(os.path.dirname(__file__))
@@ -6,7 +6,17 @@ if __name__ == "__main__":
 		sys.path.append(os.path.abspath(p+"/../.."))
 	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 
-
+pods ={}
+def search(t):
+	for e in pods:
+		#print entities[e]['title'], t
+		e_t = pods[e]
+		o_t = t.strip()
+		s = difflib.SequenceMatcher(None, o_t, e_t).ratio()
+		if(s>0.9):
+			return e
+	print t
+	return None
 
 def prepare_paper_json():
 	f = open('data/sigmod2013/prog_ck.csv','rU')
@@ -25,6 +35,7 @@ def prepare_paper_json():
 					paper_id = 'sig'+re.search(r'\d+', row[1]).group()
 				elif(row[1].startswith('pods')):
 					paper_id = 'pods'+re.search(r'\d+', row[1]).group()
+					pods[paper_id] = unicode(row[2], "ISO-8859-1")
 				else:
 					paper_id = row[1]
 				title = unicode(row[2], "ISO-8859-1")
@@ -78,7 +89,7 @@ def prepare_session_json():
 		else:
 			if(row[1]!='' and row[2] == '' and row[0]==''):
 				submissions = []
-				session_id = row[1][:row[1].index(':')]
+				session_id = row[1][:row[1].index(':')].replace(' ','')
 				s_title = unicode(row[1], "ISO-8859-1").strip()
 				sessions[session_id]={'s_title': s_title, 'submissions':submissions}
 			else:
@@ -94,7 +105,7 @@ def prepare_session_json():
 		else:
 			if(row[1]!='' and row[2] == '' and row[0]==''):
 				submissions = []
-				session_id = row[1][:row[1].index(':')]
+				session_id = row[1][:row[1].index(':')].replace(' ','')
 				s_title = unicode(row[1], "ISO-8859-1").strip()
 				sessions[session_id]={'s_title': s_title, 'submissions':submissions}
 			else:
@@ -110,10 +121,26 @@ def prepare_session_json():
 		else:
 			if(row[1]!=''):
 				submissions = []
-				session_id = row[1][:row[1].index(':')]
+				session_id = row[1][:row[1].index(':')].replace(' ','')
 				s_title = unicode(row[1], "ISO-8859-1").strip()
 				submissions.append('sig%03d' %(int(row[0])))
 				sessions[session_id]={'s_title': s_title, 'submissions':submissions}
+	
+	p = open('server/static/json/sigmod2013/sessions.json','w')
+	p.write('sessions='+json.dumps(sessions))
+
+	f = open('data/sigmod2013/PODS13-papers.json','rU')
+	pdos_papers = {}
+	data = json.loads(f.read())
+	for row in data:
+		pdos_papers[row['id']]=row['title']
+	f = open('data/sigmod2013/PODS13-session.json','rU')
+	data = json.loads(f.read())
+	for row in data:		
+		submissions = [search(pdos_papers[x]) for x in row['submissions']]
+		session_id = 'PODS'+str(row['id'])
+		s_title = row['title'].strip()
+		sessions[session_id]={'s_title': s_title, 'submissions':submissions}
 	
 	p = open('server/static/json/sigmod2013/sessions.json','w')
 	p.write('sessions='+json.dumps(sessions))
