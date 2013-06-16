404b7498c9d39ced670514c9ad5e19cdea8eb905
Anant Bhardwaj <anantb@csail.mit.edu>
2013-06-16 11:39:21 -0700

    more things

--- server/views.py
+++ server/views.py
@@ -15,8 +15,8 @@ from schema.models import *
 p = os.path.abspath(os.path.dirname(__file__))
 if(os.path.abspath(p+"/..") not in sys.path):
 	sys.path.append(os.path.abspath(p+"/.."))
-if(os.path.abspath(p+"/production/confer") not in sys.path):
-	sys.path.append(os.path.abspath(p+"/production/confer"))
+
+os.environ["PATH"] += os.pathsep + "/production/confer"
 
 
 '''
