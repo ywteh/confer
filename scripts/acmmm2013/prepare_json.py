9618947016c3e8837ce6bcf962e734ba5a5eb675
Anant Bhardwaj <anantb@csail.mit.edu>
2013-10-16 09:01:36 -0400

    new ACMMM'13 data

--- scripts/acmmm2013/prepare_json.py
+++ scripts/acmmm2013/prepare_json.py
@@ -19,6 +19,7 @@ def load_abstracts():
 	for row in rows:
 		row = unicode(row, "ISO-8859-1")
 		data = row.split('\t')
+		print data
 		if data[11] in paper_type:
 			papers[data[0]] = {'title': data[1], 'authors': [{'name': name} for name in data[2].strip('"').split(',')], 'abstract': data[3]}
 			if(data[5] in sessions):
