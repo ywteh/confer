050c9d91715e4caa0da00f3291b1bf6f88f23b8e
Anant Bhardwaj <anantb@csail.mit.edu>
2013-11-02 01:49:42 -0400

    Log Out

--- server/auth.py
+++ server/auth.py
@@ -150,7 +150,12 @@ def logout (request):
     del request.session[kLogIn]
   if kName in request.session.keys():
     del request.session[kName]
-  return HttpResponseRedirect('/')
+  c = {
+    'msg_title': 'Thank you for using Confer!',
+    'msg_body': 'Your have been logged out.<ul><li><a class= "blue bold" href="/home">Click Here</a> to browse confer as guest.</li><li><a class= "blue bold" href="/login">Click Here</a> to log in again.</li></ul>'
+  } 
+  c.update(csrf(request))
+  return render_to_response('confirmation.html', c)
 
 
 def forgot (request):
