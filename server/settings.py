e5ae720f10ba78905f57f9fe699a0e2d54005296
Anant Bhardwaj <anantb@csail.mit.edu>
2013-11-03 01:38:56 -0400

    fix DEBUG mode

--- server/settings.py
+++ server/settings.py
@@ -1,9 +1,9 @@
 # Django settings for confer project.
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-#ALLOWED_HOSTS = ['confer.csail.mit.edu', 'confer-experimental.csail.mit.edu', 'localhost']
+ALLOWED_HOSTS = ['confer.csail.mit.edu', 'confer-experimental.csail.mit.edu', 'localhost']
 
 ADMINS = (
     ('Anant Bhardwaj', 'anantb@csail.mit.edu'),
