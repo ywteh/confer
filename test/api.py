36fae0dd9d7e73052a30311efb7c9b7348ae1144
Anant Bhardwaj <anantb@csail.mit.edu>
2014-01-21 11:05:28 -0500

    more formatting

--- test/api.py
+++ test/api.py
@@ -1,16 +1,34 @@
 import httplib, urllib, json
 
+'''
+@author: Anant Bhardwaj
+@date: Jan 13, 2014
 
-def http_post(url, params):
-	conn = httplib.HTTPConnection("confer.csail.mit.edu")
-	headers = {"Content-type": "application/x-www-form-urlencoded",
-			"Accept": "text/plain"}
+A sample of how to use confer APIs
+'''
+
+
+def http_post(host, path, params):
+	conn = httplib.HTTPConnection(host)
+	headers = {
+      "Content-type": "application/x-www-form-urlencoded",
+			"Accept": "text/plain"
+    }
 	params = urllib.urlencode(params)
-	conn.request("POST", url, params, headers)	
+	conn.request("POST", path, params, headers)	
 	res = conn.getresponse().read()
 	return json.loads(res)
 
+print http_post('confer.csail.mit.edu', '/api/likes', {
+    'login_id': 'anantb@csail.mit.edu',
+    'conf_id': 'cscw2014', 'application_id':
+    'common_ties',
+    'application_token': 'xxx'
+  })
 
-
-print http_post('/api/likes', {'login_id': 'anantb@csail.mit.edu', 'conf_id': 'cscw2014', 'application_id': 'common_ties', 'application_token': 'xxx'})
-print http_post('/api/similar_people', {'login_id': 'anantb@csail.mit.edu', 'conf_id': 'cscw2014', 'application_id': 'common_ties', 'application_token': 'xxx'})
+print http_post('confer.csail.mit.edu', '/api/similar_people', {
+    'login_id': 'anantb@csail.mit.edu',
+    'conf_id': 'cscw2014', 'application_id':
+    'common_ties',
+    'application_token': 'xxx'
+  })
