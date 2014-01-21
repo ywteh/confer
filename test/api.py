c6a23c3c212e657a6a58ddc0854fc6f519a6e02d
Anant Bhardwaj <anantb@csail.mit.edu>
2014-01-21 10:45:36 -0500

    adding API test

---
+++ test/api.py
@@ -0,0 +1,16 @@
+import httplib, urllib, json
+
+
+def http_post(url, params):
+	conn = httplib.HTTPConnection("confer.csail.mit.edu")
+	headers = {"Content-type": "application/x-www-form-urlencoded",
+			"Accept": "text/plain"}
+	params = urllib.urlencode(params)
+	conn.request("POST", url, params, headers)	
+	res = conn.getresponse().read()
+	return json.loads(res)
+
+
+
+print http_post('/api/likes', {'login_id': 'anantb@csail.mit.edu', 'conf_id': 'cscw2014'})
+print http_post('/api/similar_people', {'login_id': 'anantb@csail.mit.edu', 'conf_id': 'cscw2014'})
