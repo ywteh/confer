25c4122aeb2e740f6547c9c0c457306c80e5b8a4
Anant Bhardwaj <anantb@csail.mit.edu>
2014-01-24 17:53:54 -0500

    add FB comment

--- server/static/javascript/confer.js
+++ server/static/javascript/confer.js
@@ -1283,7 +1283,6 @@ function load_paper(){
   console.log(paper_id)
   var selected_paper_html = get_selected_paper_html(paper_id)
   $('#selected_paper').find('.form').html(selected_paper_html)
-  <div class="fb-comments" data-href="http://example.com/comments" data-numposts="1" data-colorscheme="light"></div>
   $('#similar_papers').html('')
   var recs = offline_recs[paper_id]
   var raw_html = ''
