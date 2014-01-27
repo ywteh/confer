d8fbf509f27b3d48688af3a4b296879be8ed87e6
Anant Bhardwaj <anantb@csail.mit.edu>
2014-01-26 22:12:15 -0500

    confer api sample php

---
+++ examples/confer-api-sample-client.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @author: Anant Bhardwaj
+ * @date: Jan 26, 2014
+ * 
+ * A sample of how to use confer APIs
+ */
+
+$params = array(
+	'login_id' => 'anantb@csail.mit.edu',
+    'conf_id' => 'cscw2014',
+    'application_id' => 'common_ties',
+    'application_token' => 'xxx');
+
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($params),
+    ),
+);
+
+$context  = stream_context_create($options);
+
+$likes = file_get_contents('http://confer.csail.mit.edu/api/likes', false, $context);
+var_dump($likes);
+
+$similar_people = file_get_contents('http://confer.csail.mit.edu/api/similar_people', false, $context);
+var_dump($similar_people);
+
+?>
