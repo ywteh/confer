dd6fb640abd569821742c647c1e41288a7e56668
Anant Bhardwaj <anantb@csail.mit.edu>
2014-02-06 00:21:56 -0500

    add comments in the examples

--- examples/confer-api-sample-client.php
+++ examples/confer-api-sample-client.php
@@ -1,12 +1,18 @@
 <?php
 
-/**
+/*
  * @author: Anant Bhardwaj
  * @date: Jan 26, 2014
  * 
  * A sample of how to use confer APIs
  */
 
+/**
+ * Step 1: Create your app at http://confer.csail.mit.edu/developer/apps (click on the button 'Create a New App')
+ * Step 2: You can get your app_token at http://confer.csail.mit.edu/developer/apps at any time
+ * Step 3: Redirect your first-time users to http://confer.csail.mit.edu/developer/allow_access?app_id=<your app id> 
+ *         so that they can allow your app to access their data.
+ */
 $params = array(
     'login_id' => 'anantb@csail.mit.edu',
     'conf_id' => 'cscw2014',
@@ -23,9 +29,17 @@ $options = array(
 
 $context  = stream_context_create($options);
 
+// get likes for a confer login_id
 $likes = file_get_contents('http://confer.csail.mit.edu/api/likes', false, $context);
 var_dump($likes);
 
+/*
+ * If you get msg = "ACCESS_DENIED" in the response, it means user has revoked the access to your app. 
+ * You can redirect the user to http://confer.csail.mit.edu/developer/allow_access?app_id=test_app to
+ * grant the access
+ */
+
+// get similar_people for a confer login_id
 $similar_people = file_get_contents('http://confer.csail.mit.edu/api/similar_people', false, $context);
 var_dump($similar_people);
 
