a2e3ba1a2cf2d27fe19b723ad41eb6ac2a7f41cc
Anant Bhardwaj <anantb@csail.mit.edu>
2014-02-06 19:52:47 -0500

    sample viz

--- server/views.py
+++ server/views.py
@@ -237,7 +237,7 @@ def paper_paper_graph (request, conf):
     registrations = Registration.objects.filter(conference=conference)
     for r in registrations:        
       r_likes = Likes.objects.get(registration=r)
-      r_papers = json.loads(r_likes)
+      r_papers = json.loads(r_likes.likes)
       for p in r_papers:
         likes[p].add(r)
         nodes.add(p)
