e1b96233ebebdce485cf73b5e8541f05df3bfe08
Anant Bhardwaj <anantb@csail.mit.edu>
2014-02-14 20:40:02 -0500

    new settings

--- server/settings.py
+++ server/settings.py
@@ -1,12 +1,9 @@
 # Django settings for confer project.
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = ['confer.csail.mit.edu', 'confer-experimental.csail.mit.edu', 'confer-staging.csail.mit.edu']
-
 ADMINS = (
-    ('Confer Team', 'confer@csail.mit.edu'),
 )
 
 
@@ -19,7 +16,7 @@ DATABASES = {
         'USER': 'postgres',                      # Not used with sqlite3.
         'PASSWORD': 'postgres',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5433',                      # Set to empty string for default. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -156,3 +153,9 @@ LOGGING = {
         },
     }
 }
+
+# server_settings
+try:
+    from server_settings import *
+except ImportError:
+    pass
