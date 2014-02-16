29d2a69b3a0af1d967f9d879fc10e4aee7ff398d
Anant Bhardwaj <anantb@csail.mit.edu>
2014-02-16 12:58:44 -0500

    database is confer-production

--- scripts/confer-db-backup.sh
+++ scripts/confer-db-backup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pg_dump -U postgres -h confer.csail.mit.edu -p 5433 -b -F c -f "$HOME/backups/confer/confer-`date +%m-%d-%Y`.backup" confer
+pg_dump -U postgres -h confer-production.csail.mit.edu -p 5433 -b -F c -f "$HOME/backups/confer/confer-`date +%m-%d-%Y`.backup" confer
