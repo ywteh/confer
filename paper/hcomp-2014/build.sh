dbc7279b29619236efcd6786891782c20a4e35b9
Anant Bhardwaj <anantb@csail.mit.edu>
2014-03-30 00:23:28 -0400

    minor changes

--- paper/hcomp-2014/build.sh
+++ paper/hcomp-2014/build.sh
@@ -2,13 +2,13 @@
 echo "preparing latex"
 python gdoc2latex.py "https://docs.google.com/document/d/1sdXb8pCzEoZjrrndDiNROg7Q5wPvBiwA9vm3muiOnKQ/edit" > confer-hcomp-14.tex
 echo "latexing"
-latex confer-hcomp-14.tex
+pdflatex confer-hcomp-14.tex
 echo "latexing once more"
-latex confer-hcomp-14.tex
+pdflatex confer-hcomp-14.tex
 
-echo "creating the PDF using"
-dvips -Ppdf -G0 -tletter confer-hcomp-14 -o confer-hcomp-14.ps
-ps2pdf -sPAPERSIZE=letter -dMaxSubsetPct=100 -dCompatibilityLevel=1.2 -dSubsetFonts=false -dEmbedAllFonts=true confer-hcomp-14.ps
-cd ..
+#echo "creating the PDF using"
+#dvips -Ppdf -G0 -tletter confer-hcomp-14 -o confer-hcomp-14.ps
+#ps2pdf -sPAPERSIZE=letter -dMaxSubsetPct=100 -dCompatibilityLevel=1.2 -dSubsetFonts=false -dEmbedAllFonts=true confer-hcomp-14.ps
+#cd ..
 
 echo "DONE"
