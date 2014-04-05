188156187d6cf341edf253836a86745eff7d1e30
Anant Bhardwaj <anantb@csail.mit.edu>
2014-04-05 01:55:06 -0400

    references

--- paper/hcomp-2014/build.sh
+++ paper/hcomp-2014/build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+rm -rf *.aux
+rm -rf *.bbl
+rm -rf *.blg
+rm -rf *.pdf
+rm -rf *.log
 echo "preparing latex"
 python gdoc2latex.py "https://docs.google.com/document/d/1sdXb8pCzEoZjrrndDiNROg7Q5wPvBiwA9vm3muiOnKQ/edit" > confer-hcomp-14.tex
 echo "latexing"
 pdflatex confer-hcomp-14.tex
+bibtex confer-hcomp-14
 echo "latexing once more"
+pdflatex confer-hcomp-14.tex
 pdflatex confer-hcomp-14.tex
 
 #echo "creating the PDF using"
