869c5170c55825f353bcb4527f801ef6874b40a9
Anant Bhardwaj <anantb@csail.mit.edu>
2014-04-28 05:32:16 -0400

    similar people

--- server/static/javascript/confer.js
+++ server/static/javascript/confer.js
@@ -1167,7 +1167,7 @@ function compute_recs(recs) {
   recommended = final_recs
 }
 
-function create_person_html(id) {
+function create_person_html(p) {
   raw_html = '<tr class="clickable">'
   raw_html += '<td class="metadata">'
   raw_html += '<div class="star star-open m_star" data="" onclick="handle_person_star(event);">'      
@@ -1176,9 +1176,9 @@ function create_person_html(id) {
 
   raw_html += '<td class="content">'
   raw_html += '<ul>'
-  raw_html += '<li><h3><a href="mailto:?to={{p.email}}&amp;subject=Hello from {{login_name}}&amp;body=Hi {{p.name}},%0A%0AConfer thinks that we two have similar research interests. Would you be interested in meeting?%0A%0A{{login_name}}" target="_blank">{{p.name}}</a></h3></li>'
+  raw_html += '<li><h3><a href="mailto:?to=' + p.email + '&amp;subject=Hello from ' + login_name + '&amp;body=Hi + ' + p.name + ',%0A%0AConfer thinks that we two have similar research interests. Would you be interested in meeting?%0A%0A'+login_name+'" target="_blank">'+p.name+'</a></h3></li>'
   raw_html += '<li><span class="paper-subtype" style="text-align: left !important;">'
-  raw_html += 'You and {{p.name}} have {{p.common_likes}} papers in common. <a class="blue" href="mailto:?to={{p.email}}&amp;subject=Hello from {{login_name}}&amp;body=Hi {{p.name}},%0A%0AConfer thinks that we two have similar research interests. Would you be interested in meeting?%0A%0A{{login_name}}" target="_blank">Say hello</a> to {{p.name}}!</li>'
+  raw_html += 'You and ' + p.name + 'have '+p.common_likes +' papers in common. <a class="blue" href="mailto:?to='+p.email+'&amp;subject=Hello from '+ login_name + '&amp;body=Hi '+p.name+',%0A%0AConfer thinks that we two have similar research interests. Would you be interested in meeting?%0A%0A'+login_name+'" target="_blank">Say hello</a> to '+p.name+'!</li>'
   raw_html += '</span>'
   raw_html += '</ul>'
   raw_html += '</td>'
@@ -1215,7 +1215,7 @@ function populate_similar_people () {
   if (similar_people.length > 0) {
     raw_html = ''
     for(p in similar_people) {
-      raw_html += create_person_html(id)
+      raw_html += create_person_html(similar_people[p])
     }
     $("#similar_people").html(raw_html)
   } else {
