a842a4f23e92f1f391e74536e1b03b0059091b00
Anant Bhardwaj <anantb@csail.mit.edu>
2015-02-26 13:00:21 -0500

    get all the graphs

--- scripts/paper-analysis/network_graph.py
+++ scripts/paper-analysis/network_graph.py
@@ -16,77 +16,6 @@ from server.models import *
 
 
 '''
-Finds all the cliques in the graph and prints various print_graph_stats
-'''
-def print_graph_stats(graph):
-  cliques = [c for c in nx.find_cliques(graph)]
-
-  num_cliques = len(cliques)
-
-  clique_sizes = [len(c) for c in cliques]
-  max_clique_size = max(clique_sizes)
-  avg_clique_size = sum(clique_sizes) / num_cliques
-
-  max_cliques = [c for c in cliques if len(c) == max_clique_size]
-
-  num_max_cliques = len(max_cliques)
-
-  max_clique_sets = [set(c) for c in max_cliques]
-  nodes_in_all_max_cliques = list(
-      reduce(lambda x, y: x.intersection(y), max_clique_sets))
-
-  print 'Num cliques:', num_cliques
-  print 'Avg clique size:', avg_clique_size
-  print 'Max clique size:', max_clique_size
-  print 'Num max cliques:', num_max_cliques
-  print
-  print 'nodes in all max cliques:'
-  print json.dumps(nodes_in_all_max_cliques, indent=1)
-  print
-  print 'Max cliques:'
-  print json.dumps(max_cliques, indent=1)
-
-
-'''
-Constructs a similarity graph from an affinity_map
-
-For Example:
-
-Input
-
-friends_affinity_map = {
-  'rob': ['david', 'daniel', 'sam'],
-  'david': ['rob', 'sam', 'anant'],
-  'sam': ['rob', 'david', 'anant']
-}
-
-Output Graph:
-
-Nodes = ['rob', 'anant', 'sam', 'david', 'daniel']
-Edges = [
-  {source: 'rob', target: 'david', 'weight': 1},
-  {source: 'rob', target: 'sam', 'weight': 1},
-  {source: 'david', target: 'sam', 'weight': 2}
-]
-'''
-def construct_similarity_graph(affinity_map):
-  network = nx.Graph()
-  for key1 in affinity_map:
-    for key2 in affinity_map:
-      if key1 == key2:
-        continue
-      
-      common_data = affinity_map[key1].intersection(affinity_map[key2])
-      weight = len(common_data)
-      
-      if weight > 0:
-        network.add_edge(
-            key1, key2, weight=weight, common_data=list(common_data))
-
-  return network
-
-
-'''
 Fetches data from the databse
 
 For a given conf, returns a dictionary of <user: set(preferences)>
@@ -114,6 +43,45 @@ def get_user_preferences (conf):
       pass
 
   return user_preferences
+
+
+'''
+Constructs a similarity graph from an affinity_map
+
+For Example:
+
+Input
+
+friends_affinity_map = {
+  'rob': ['david', 'daniel', 'sam'],
+  'david': ['rob', 'sam', 'anant'],
+  'sam': ['rob', 'david', 'anant']
+}
+
+Output Graph:
+
+Nodes = ['rob', 'anant', 'sam', 'david', 'daniel']
+Edges = [
+  {source: 'rob', target: 'david', 'weight': 1},
+  {source: 'rob', target: 'sam', 'weight': 1},
+  {source: 'david', target: 'sam', 'weight': 2}
+]
+'''
+def construct_similarity_graph(affinity_map):
+  similarity_graph = nx.Graph()
+  for key1 in affinity_map:
+    for key2 in affinity_map:
+      if key1 == key2:
+        continue
+      
+      common_data = affinity_map[key1].intersection(affinity_map[key2])
+      weight = len(common_data)
+      
+      if weight > 0:
+        similarity_graph.add_edge(
+            key1, key2, weight=weight, common_data=list(common_data))
+
+  return similarity_graph
 
 
 '''
@@ -153,6 +121,38 @@ def get_network_graph (conf):
     'paper_paper_similarity_graph': paper_paper_similarity_graph,
     'person_person_similarity_graph': person_person_similarity_graph
   }
+
+
+'''
+Finds all the cliques in the graph and prints various print_graph_stats
+'''
+def print_graph_stats(graph):
+  cliques = [c for c in nx.find_cliques(graph)]
+
+  num_cliques = len(cliques)
+
+  clique_sizes = [len(c) for c in cliques]
+  max_clique_size = max(clique_sizes)
+  avg_clique_size = sum(clique_sizes) / num_cliques
+
+  max_cliques = [c for c in cliques if len(c) == max_clique_size]
+
+  num_max_cliques = len(max_cliques)
+
+  max_clique_sets = [set(c) for c in max_cliques]
+  nodes_in_all_max_cliques = list(
+      reduce(lambda x, y: x.intersection(y), max_clique_sets))
+
+  print 'Num cliques:', num_cliques
+  print 'Avg clique size:', avg_clique_size
+  print 'Max clique size:', max_clique_size
+  print 'Num max cliques:', num_max_cliques
+  print
+  print 'nodes in all max cliques:'
+  print json.dumps(nodes_in_all_max_cliques, indent=1)
+  print
+  print 'Max cliques:'
+  print json.dumps(max_cliques, indent=1)
 
 
 def main():
