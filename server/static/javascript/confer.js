d9eac999b5bcbd281b79f1324ec0c1539739eada
Anant Bhardwaj <anantb@csail.mit.edu>
2015-03-15 19:31:37 -0400

    add friendly option

--- server/static/javascript/confer.js
+++ server/static/javascript/confer.js
@@ -1256,7 +1256,6 @@ function populate_similar_people () {
 function highlight_favorite(){
   $('.m_star').removeClass('star-filled').addClass('star-open')
   for(p in people_you_favorited) {
-    $('.user_' + people_you_favorited[p].id).removeClass('star-open').addClass('star-filled')
     $('#similar_people').find('.user_' + people_you_favorited[p].id).parent().parent().hide()
   }
 }
