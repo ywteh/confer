fe6e9242438fdd606628b9c25f6e73a35fdee9a2
anantb <anantb@csail.mit.edu>
2016-04-06 23:16:03 -0700

    SIGMOD 2016

--- scripts/sigmod2016/prepare_json.py
+++ scripts/sigmod2016/prepare_json.py
@@ -11,8 +11,8 @@ def prepare_papers(data_file):
   for row in reader:
     paper_id = unicode(row[0], "ISO-8859-1")
     paper_title = unicode(row[1], "ISO-8859-1")
-    paper_type = unicode(row[2], "ISO-8859-1")
-    abstract = unicode(row[3], "ISO-8859-1")
+    paper_type = unicode(row[2], "ISO-8859-1").split(',', 1)[0].strip()
+    abstract = unicode(row[3], "ISO-8859-1").split(',', 1)[0].strip()
     paper_authors = unicode(row[4], "ISO-8859-1")   
     
 
